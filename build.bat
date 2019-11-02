9c60ed2ef1e2c3b93a3099a6938575eacad6beb2
no7mac <44577325+no7macs@users.noreply.github.com>
2019-11-02 17:38:49 -0600

    Add files via upload

--- build.bat
+++ build.bat
@@ -10,12 +10,12 @@ timeout 2
 copy "%mypath%\Data\Images\" "%mypath%\build\program\Data\Images\"
 
 timeout 2
-pyinstaller --workpath ./build/working --distpath ./build/program/data/Hotkey --specpath ./build --ascii --noconfirm ./Data/Hotkey/HotKey.pyw
+pyinstaller --workpath ./build/working --distpath ./build/program/data/Hotkey --specpath ./build --ascii --noconfirm --clean ./Data/Hotkey/HotKey.pyw
 
-pyinstaller --workpath ./build/working --distpath ./build/program/data/Settings --specpath ./build --ascii --noconfirm ./Data/Settings/Settings.pyw
+pyinstaller --workpath ./build/working --distpath ./build/program/data/Settings --specpath ./build --ascii --noconfirm --clean ./Data/Settings/Settings.pyw
 timeout 2 
 
-pyinstaller --workpath ./build/working --distpath ./build/program --specpath ./build --runtime-tmpdir ./Data/tmp --ascii --noupx --noconfirm ./Open_Clicker.pyw -wF
+pyinstaller --workpath ./build/working --distpath ./build/program --specpath ./build --runtime-tmpdir ./Data/tmp --ascii --noupx --noconfirm --clean ./Open_Clicker.pyw -wF
 timeout 2
 
 timeout 10
