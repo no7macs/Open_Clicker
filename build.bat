4f5da6ac2c695102f1596828b55bf4a805b295ce
no7mac <44577325+no7macs@users.noreply.github.com>
2019-11-02 17:24:20 -0600

    better compile file

--- build.bat
+++ build.bat
@@ -10,13 +10,12 @@ timeout 2
 copy "%mypath%\Data\Images\" "%mypath%\build\program\Data\Images\"
 
 timeout 2
-pyinstaller --workpath ./build/working --distpath ./build/program --specpath ./build --ascii --key test ./Open_Clicker.pyw -wF
+pyinstaller --workpath ./build/working --distpath ./build/program/data/Hotkey --specpath ./build --ascii --noconfirm ./Data/Hotkey/HotKey.pyw
 
-timeout 2
-pyinstaller --workpath ./build/working --distpath ./build/program/data/Hotkey --specpath ./build --ascii ./Data/Hotkey/HotKey.pyw  
-
+pyinstaller --workpath ./build/working --distpath ./build/program/data/Settings --specpath ./build --ascii --noconfirm ./Data/Settings/Settings.pyw
 timeout 2 
-pyinstaller --workpath ./build/working --distpath ./build/program/data/Settings --specpath ./build --ascii ./Data/Settings/Settings.pyw
 
+pyinstaller --workpath ./build/working --distpath ./build/program --specpath ./build --runtime-tmpdir ./Data/tmp --ascii --noupx --noconfirm ./Open_Clicker.pyw -wF
+timeout 2
 
 timeout 10
