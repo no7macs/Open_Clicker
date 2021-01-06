7e7995149a481498ba8748981d2c75848d26a773
no7mac <44577325+no7macs@users.noreply.github.com>
2021-01-05 17:44:01 -0700

    some of general section gui

--- Settings.py
+++ Settings.py
@@ -1,9 +1,11 @@
 from tkinter import *
 import json, gc
 
-def save(loadedjsonsettings):
+def save(loadedjsonsettings, minValueEntry, maxValueEntry):
     loadedjsonsettings['settings']['runOnStartup'] = bool()
     loadedjsonsettings['settings']['startMinimize'] = bool()
+    loadedjsonsettings['mincpsval'] = int(minValueEntry.get())
+    loadedjsonsettings['maxcpsval'] = int(maxValueEntry.get())
 
     with open('./json_settings.json','w') as settingsfile:
         json.dump(loadedjsonsettings, settingsfile, indent=4)
@@ -32,8 +34,46 @@ class generalSettingsWindow(Frame):
     def __init__(self, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
 
-class mainView(Frame):
+        #Change cps limits
+        changeCPSLimitFrame = Frame(self, bg = '#0F151D', highlightbackground="#C96C00", highlightthicknes=1, width=10000)
+        changeCPSLimitFrame.pack(side=TOP)
+        changeCPSLimitLabel = Label(changeCPSLimitFrame, text='change CPS slider limits', bg = '#0F151D', fg = '#C96C00')
+        changeCPSLimitLabel.pack(side=TOP)
+        minValueLabel = Label(changeCPSLimitFrame, text = 'Minimum Value (in milliseconds)', bg = '#0F151D', fg = '#C96C00')
+        minValueLabel.pack(side=TOP)
+        minValueEntry = Entry(changeCPSLimitFrame, bg = '#2B2D31', fg='#C96C00', insertbackground = '#C96C00')
+        minValueEntry.pack(side=TOP)
+        maxValueLabel = Label(changeCPSLimitFrame, text = 'Maximum Value (in milliseconds)', bg = '#0F151D', fg = '#C96C00')
+        maxValueLabel.pack(side=TOP)
+        maxValueEntry = Entry(changeCPSLimitFrame, bg = '#2B2D31', fg='#C96C00', insertbackground = '#C96C00')
+        maxValueEntry.pack(side=TOP)
 
+        #Startup and running stuff
+        runningStuffFrame = Frame(self, bg = '#0F151D', highlightbackground="#C96C00", highlightthicknes=1)
+        runningStuffFrame.pack(side=TOP)
+        runningTitlesFrame = Frame(runningStuffFrame, bg='#0F151D')
+        runningTitlesFrame.pack(side=LEFT)
+        runningInputsFrame = Frame(runningStuffFrame, bg='#0F151D')
+        runningInputsFrame.pack(side=LEFT)
+        #Labels
+        runOnStartupLabel = Label(runningTitlesFrame, text='run on startup: ', bg = '#0F151D', fg = '#C96C00')
+        runOnStartupLabel.pack(anchor=E)
+        startMinimizedLabel = Label(runningTitlesFrame, text='start minimized: ', bg = '#0F151D', fg = '#C96C00')
+        startMinimizedLabel.pack(anchor=E)
+        runWithElevatedPrivligesLabel = Label(runningTitlesFrame, text='run with elevated privliges: ', bg = '#0F151D', fg = '#C96C00')
+        runWithElevatedPrivligesLabel.pack(anchor=E)
+        #Checkboxes
+        runOnStartupVar = IntVar()
+        runOnStartupCheckbox = Checkbutton(runningInputsFrame, variable=runOnStartupVar, onvalue=True, offvalue=False, bg='#0F151D', fg='#C96C00', highlightbackground='#0F151D', highlightcolor='#1E1D1D', selectcolor='#0F151D', activebackground='#0F151D', activeforeground='#066D9F')
+        runOnStartupCheckbox.pack(anchor=W)
+        startMinimizedVar = IntVar()
+        startMinimizedCheckbox = Checkbutton(runningInputsFrame, variable=startMinimizedVar, onvalue=True, offvalue=False, bg='#0F151D', fg='#C96C00', highlightbackground='#0F151D', highlightcolor='#1E1D1D', selectcolor='#0F151D', activebackground='#0F151D', activeforeground='#066D9F')
+        startMinimizedCheckbox.pack(anchor=W)
+        runWithElevatedPrivligesVar = IntVar()
+        runWithElevatedPrivligesCheckbox = Checkbutton(runningInputsFrame, variable=runWithElevatedPrivligesVar, onvalue=True, offvalue=False, bg='#0F151D', fg='#C96C00', highlightbackground='#0F151D', highlightcolor='#1E1D1D', selectcolor='#0F151D', activebackground='#0F151D', activeforeground='#066D9F')
+        runWithElevatedPrivligesCheckbox.pack(anchor=W)
+
+class mainView(Frame):
     def __init__(self, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
         buttons = Frame(self, bg = '#0F151D')
@@ -46,7 +86,7 @@ class mainView(Frame):
         container.pack(side=TOP)
 
         currentPageLabel = Label(currentPageFrame, text='n/a', bg = '#0F151D', fg = '#C96C00')
-        currentPageLabel.pack(anchor=W)
+        currentPageLabel.pack(side=TOP)
 
         hotKeys = hotKeyWindow(self, bg='#0F151D')
         generalSettings = generalSettingsWindow(self, bg='#0F151D')
@@ -65,6 +105,8 @@ class mainView(Frame):
         hotKeysButton = Button(buttons, justify=LEFT, command=lambda:self.change(currentPageLabel, 'HotKeys', hotKeyWindow), image=hotKeyButtonPhoto, bg = '#2B2D31', fg='#C96C00', activebackground='#1E1B15', activeforeground='#066D9F')
         hotKeysButton.image = hotKeyButtonPhoto
         hotKeysButton.pack(anchor = E)
+
+        self.change(currentPageLabel, 'General', generalSettingsWindow)
 
     def change(self, currentPageLabel, displayName, className):
         currentPageLabel.config(text=displayName)
