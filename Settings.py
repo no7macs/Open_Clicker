96f323d2b182216e9adbc2519cb3a16d9f46108c
no7mac <44577325+no7macs@users.noreply.github.com>
2021-01-08 23:05:53 -0700

    jank fix to toggling buttons cus I got tiredof finding a better alternative

--- Settings.py
+++ Settings.py
@@ -11,7 +11,7 @@ with open('./json_settings.json','r') as settingsfile:
     loadedjsonsettings = json.loads(jsondata)
     settingsfile.close()
 
-VK_CODE = {'backspace':0x08, 'tab':0x09, 'clear':0x0C, 'enter':0x0D, 'shift':0x10, 'ctrl':0x11, 'alt':0x12, 'pause':0x13, 'caps_lock':0x14, 'esc':0x1B,
+VK_CODE = {'backspace':0x08, 'tab':0x09, 'clear':0x0C, 'enter':0x0D, 'alt':0x12, 'pause':0x13, 'caps_lock':0x14, 'esc':0x1B,
     'spacebar':0x20, 'page_up':0x21, 'page_down':0x22, 'end':0x23, 'home':0x24, 'left_arrow':0x25, 'up_arrow':0x26, 'right_arrow':0x27, 'down_arrow':0x28,
     'select':0x29, 'print':0x2A, 'execute':0x2B, 'print_screen':0x2C, 'ins':0x2D, 'del':0x2E, 'help':0x2F, '0':0x30, '1':0x31, '2':0x32, '3':0x33,
     '4':0x34, '5':0x35, '6':0x36, '7':0x37, '8':0x38, '9':0x39, 'a':0x41, 'b':0x42, 'c':0x43, 'd':0x44, 'e':0x45, 'f':0x46, 'g':0x47, 'h':0x48,
@@ -20,7 +20,7 @@ VK_CODE = {'backspace':0x08, 'tab':0x09, 'clear':0x0C, 'enter':0x0D, 'shift':0x1
     'numpad_6':0x66, 'numpad_7':0x67, 'numpad_8':0x68, 'numpad_9':0x69, 'multiply_key':0x6A, 'add_key':0x6B, 'separator_key':0x6C, 'subtract_key':0x6D,
     'decimal_key':0x6E, 'divide_key':0x6F, 'F1':0x70, 'F2':0x71, 'F3':0x72, 'F4':0x73, 'F5':0x74, 'F6':0x75, 'F7':0x76, 'F8':0x77, 'F9':0x78,
     'F10':0x79, 'F11':0x7A, 'F12':0x7B, 'F13':0x7C, 'F14':0x7D, 'F15':0x7E, 'F16':0x7F, 'F17':0x80, 'F18':0x81, 'F19':0x82, 'F20':0x83, 'F21':0x84,
-    'F22':0x85, 'F23':0x86, 'F24':0x87, 'num_lock':0x90, 'scroll_lock':0x91, 'left_control':0xA2, 'right_control':0xA3,
+    'F22':0x85, 'F23':0x86, 'F24':0x87, 'num_lock':0x90, 'scroll_lock':0x91, 'left_shift':0xA0, 'right_shift':0xA1, 'left_control':0xA2, 'right_control':0xA3,
     'left_menu':0xA4, 'right_menu':0xA5, 'browser_back':0xA6, 'browser_forward':0xA7, 'browser_refresh':0xA8, 'browser_stop':0xA9, 'browser_search':0xAA,
     'browser_favorites':0xAB, 'browser_start_and_home':0xAC, 'volume_mute':0xAD, 'volume_Down':0xAE, 'volume_up':0xAF, 'next_track':0xB0, 'previous_track':0xB1,
     'stop_media':0xB2, 'play/pause_media':0xB3, 'start_mail':0xB4, 'select_media':0xB5, 'start_application_1':0xB6, 'start_application_2':0xB7, 'attn_key':0xF6,
@@ -55,63 +55,66 @@ class hotKeyWindow(Frame):
         for a in self.widgetsInfo:
             self.widgetsInfo[a]['labels'][0] = Label(hotKeyLabelFrame, text=self.widgetsInfo[a]['displayName'], bg = '#0F151D', fg = '#C96C00', pady=3)
             self.widgetsInfo[a]['labels'][0].pack(anchor=W)
-            self.widgetsInfo[a]['labels'][1] = Label(currentHotKeyFrame, text=loadedjsonsettings['settings']['hotKeys']+self.widgetsInfo[a]['jsonData'], bg = '#2B2D31', fg='#C96C00', relief='ridge', width=25, pady=3)
+            self.widgetsInfo[a]['labels'][1] = Label(currentHotKeyFrame, text=loadedjsonsettings['settings']['hotKeys'][self.widgetsInfo[a]['jsonData']], bg = '#2B2D31', fg='#C96C00', relief='ridge', width=25, pady=3)
             self.widgetsInfo[a]['labels'][1].pack(anchor=W)
             self.widgetsInfo[a]['buttons'][0] = self.initSetButton(a, self.widgetsInfo)
             self.widgetsInfo[a]['buttons'][0], self.widgetsInfo = self.widgetsInfo[a]['buttons'][0].create(setChangeButtonFrame, text='Change', changeText='Set')
             self.widgetsInfo[a]['buttons'][0].pack(anchor=W)
             #self.widgetsInfo[a]['buttons'][1] = Button(cancelResetButtonFrame, text = 'Reset', command=lambda:self.change(a), bg = '#2B2D31', fg='#C96C00', activebackground='#1E1B15', activeforeground='#066D9F', width=8)
             #self.widgetsInfo[a]['buttons'][1].pack(anchor=W)
-            
+
+    def save(self):
+        for a in self.widgetsInfo:
+            loadedjsonsettings['settings']['hotKeys'][self.widgetsInfo[a]['jsonData']] = (self.widgetsInfo[a]['labels'][1]).cget('text')  
+        with open('./json_settings.json','w') as settingsfile:
+            json.dump(loadedjsonsettings, settingsfile, indent=4)
+            settingsfile.close()
+        return
+
     class initSetButton():
         def __init__(self, a, widgetsInfo):
             self.a = a
             self.widgetsInfo = widgetsInfo
             return
+
         def create(self, frame, text='n/a', changeText='n/a'):
             b = Button(frame, text=text, command=lambda:self.change(self.a, text=text, changedText=changeText), bg = '#2B2D31', fg='#C96C00', activebackground='#1E1B15', activeforeground='#066D9F', width=8)
             return(b, self.widgetsInfo)
 
+        buttonToggle = False
         def change(self, selected, text='n/a', changedText='n/a'):
             #selectedInfo = self.changeInfo.get(selected)
-            self.listner = self.keyListner(self.widgetsInfo, self.a)
-            p = multiprocessing.Process(target=self.listner.listen())
+            p = multiprocessing.Process(target=self.listen())
             print(selected)
-            for b in self.widgetsInfo:
-                if self.widgetsInfo[b]['name'] != selected:
-                    print(b)
-                    (self.widgetsInfo[b]['buttons'][0])['state'] = DISABLED
-                else: 
-                    if (self.widgetsInfo[b]['buttons'][0]).cget('text') != changedText: 
-                        (self.widgetsInfo[b]['buttons'][0]).config(text=changedText, width=8)
-                        p.start
-                    else:
-                        (self.widgetsInfo[b]['buttons'][0]).config(text=text, width=8)
+            if self.buttonToggle == False:
+                for b in self.widgetsInfo:               
+                    if self.widgetsInfo[b]['name'] == selected:
+                        (self.widgetsInfo[b]['buttons'][0])['state'] = NORMAL
+                        (self.widgetsInfo[b]['buttons'][0]).config(text=changedText)
+                    elif self.widgetsInfo[b]['name'] != selected:
+                        (self.widgetsInfo[b]['buttons'][0])['state'] = DISABLED
+                        (self.widgetsInfo[b]['buttons'][0]).config(text=text)
+                self.buttonToggle = True
+            elif self.buttonToggle == True:
+                for b in self.widgetsInfo:   
+                    if self.widgetsInfo[b]['name'] == selected:
+                        (self.widgetsInfo[b]['buttons'][0])['state'] = NORMAL
+                        (self.widgetsInfo[b]['buttons'][0]).config(text=text)
+                    elif self.widgetsInfo[b]['name'] != selected:
+                        (self.widgetsInfo[b]['buttons'][0])['state'] = NORMAL
+                        (self.widgetsInfo[b]['buttons'][0]).config(text=text)
+                self.buttonToggle = False
+        
 
-        class keyListner():
-            def __init__(self, widgetsInfo, a):
-                self.charList = []
-                self.widgetsInfo = widgetsInfo
-                self.a = a
-                return
-            def listen(self):
-                #while True:
-                for a in VK_CODE:
-                    key = win32api.GetAsyncKeyState(VK_CODE[a])
-                    if key != 0:
-                        if not a in self.charList:
-                            self.charList.append(a)
-                            (self.widgetsInfo[self.a]['labels'][1]).config(text='+'.join(self.charList))
-                            (loadedjsonsettings['settings']['hotKeys'][str(self.widgetsInfo[a]['jsonData'])] = '+'.join(self.charList)
-
-    def save(self):
-        for a in self.widgetsInfo:
-            loadedjsonsettings['settings']['hotKeys']+self.widgetsInfo[a]['jsonData'] = (self.widgetsInfo[self.a]['labels'][1]).cget('')
-
-        with open('./json_settings.json','w') as settingsfile:
-            json.dump(loadedjsonsettings, settingsfile, indent=4)
-            settingsfile.close()
-        return
+        charList = []
+        def listen(self):
+            #while True:
+            for a in VK_CODE:
+                key = win32api.GetAsyncKeyState(VK_CODE[a])
+                if key != 0:
+                    if not a in self.charList:
+                        self.charList.append(a)
+                        (self.widgetsInfo[self.a]['labels'][1]).config(text='+'.join(self.charList))
 
 class generalSettingsWindow(Frame):
     def __init__(self, *args, **kwargs):
